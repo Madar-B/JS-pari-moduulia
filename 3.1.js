9060c7c295bc8db813c83fe0d0346c52f1ca4d04
YOUR_NAME <YOUR_EMAIL>
2024-12-13 11:43:15 +0200

    Initial commit

---
+++ 3.1.js
@@ -0,0 +1,10 @@
+const targetElement = document.getElementById('target');
+
+        const newItems = `
+            <li>First item</li>
+            <li>Second item</li>
+            <li>Third item</li>
+        `;
+
+
+        targetElement.innerHTML += newItems;
