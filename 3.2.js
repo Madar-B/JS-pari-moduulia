afff7bc0cc79a2544e2ee59825e8945ff7bd65c1
YOUR_NAME <YOUR_EMAIL>
2024-12-13 11:48:01 +0200

    Initial commit

---
+++ 3.2.js
@@ -0,0 +1,23 @@
+
+
+const target = document.getElementById("target");
+
+const firstItem = document.createElement('li');
+
+firstItem.textContent = 'First Item';
+
+target.appendChild(firstItem);
+
+
+const secondItem = document.createElement('li');
+
+secondItem.textContent = ('Second Item');
+
+target.appendChild(secondItem)
+
+
+const thirdChild = document.createElement('li');
+
+thirdChild.textContent = ('Third item')
+
+target.appendChild(thirdChild)
